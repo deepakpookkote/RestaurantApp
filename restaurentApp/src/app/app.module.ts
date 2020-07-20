3e007bf597fc1b508372bf6737efcf3fa997ae34
Deepak Pookkote <deepak.pookkote@wallstreetenglish.com>
2020-07-21 01:33:32 +0530

    added reactive and template driven forms

--- restaurentApp/src/app/app.module.ts
+++ restaurentApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +31,9 @@ import { RecipeEditComponent } from './features/recipe/recipe-edit/recipe-edit.c
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- restaurentApp/src/app/features/shopping-list/shopping.service.ts
+++ restaurentApp/src/app/features/shopping-list/shopping.service.ts
@@ -8,6 +8,7 @@ import { Subject } from 'rxjs';
 export class ShoppingService {
   // ingredientChanged = new EventEmitter<Ingredient[]>();
   ingredientChanged = new Subject<Ingredient[]>();
+  initiateEditing = new Subject<number>();
 
   ingredients: Ingredient[] = [
     new Ingredient('Apple', 10),
@@ -22,8 +23,22 @@ export class ShoppingService {
     return this.ingredients;
   }
 
+  getIngredient(index: number){
+    return this.ingredients[index];
+  }
+
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientChanged.next(this.ingredients.slice());
+  }
+
+  removeIngredient(index: number) {
+    this.ingredients.splice(index, 1);
     this.ingredientChanged.next(this.ingredients.slice());
   }
 
